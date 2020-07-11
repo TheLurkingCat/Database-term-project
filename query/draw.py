255e1652126c81ae7bfd320e8c895a999ca2d894
P.T.Liu <meow.cs07@nctu.edu.tw>
2020-07-11 11:58:19 +0800

    Add data comparison

--- query/draw.py
+++ query/draw.py
@@ -1,25 +1,33 @@
 from datetime import date
+from typing import List, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
 from dateutil.relativedelta import relativedelta
 
 
-def get_data(start: date, end: date, use_month: bool) -> tuple:
+def get_data(start: date, end: date, use_month: bool) -> List[Tuple[List[float]]]:
     if use_month:
         delta = relativedelta(months=1)
         fmt = "%Y/%m"
     else:
         fmt = "%Y"
         delta = relativedelta(years=1)
-    horizontal = []
-    while start < end:
-        horizontal.append(start.strftime(fmt))
-        start += delta
-    return horizontal, np.random.randn(len(horizontal))
+
+    ret = []
+
+    for _ in range(3):
+        temp = start
+        horizontal = []
+        while temp < end:
+            horizontal.append(temp.strftime(fmt))
+            temp += delta
+        ret.append((horizontal, np.random.randn(len(horizontal))))
+    return ret
 
 
-def draw(start: date, end: date, use_bar=False, use_month=True):
+def draw(start: date, end: date, use_bar=False, use_month=True) -> None:
+    style = ('solid', 'dashed', 'dashdot', 'dotted')
     if use_bar:
         plot_function = plt.bar
         kwargs = {"color": 'k'}
@@ -27,26 +35,35 @@ def draw(start: date, end: date, use_bar=False, use_month=True):
         plot_function = plt.plot
         kwargs = {"color": 'k', "marker": '.'}
 
-    data = get_data(start, end, use_month)
     plt.subplot(2, 1, 1)
-    plot_function(*data, **kwargs)
+    dataset = get_data(start, end, use_month)
+    for idx, data in enumerate(dataset):
+        plot_function(*data, linestyle=style[idx % 4], **kwargs)
     plt.xticks(rotation=45)
+
     plt.subplot(2, 2, 3)
     kwargs['color'] = 'r'
-    plot_function(*data, **kwargs)
+    dataset = get_data(start, end, use_month)
+    for idx, data in enumerate(dataset):
+        plot_function(*data,  linestyle=style[idx % 4], **kwargs)
     plt.xticks(rotation=45)
+
     plt.subplot(2, 2, 4)
     kwargs['color'] = 'b'
-    plot_function(*data, **kwargs)
+    dataset = get_data(start, end, use_month)
+    for idx, data in enumerate(dataset):
+        plot_function(*data, linestyle=style[idx % 4], **kwargs)
     plt.xticks(rotation=45)
     plt.show()
+
 
 def main():
     start = date(1975, 1, 1)
     end = date(2020, 12, 31)
     draw(start, end, False, False)
 
+
 if __name__ == '__main__':
-    figure = plt.figure(figsize=(16, 9))
+    figure = plt.figure("Global temperature", figsize=(16, 9))
     figure.set_tight_layout({"pad": .5})
     main()
